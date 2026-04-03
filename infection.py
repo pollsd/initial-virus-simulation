9fd047c09a5093f5e2ba4c9d16a5cd0ba692a55c
pollsd <31497675+pollsd@users.noreply.github.com>
2026-04-02 22:15:22 -0500

    Add project description and simulation goals
    Added project description and simulation objectives for virus spread among employees.

--- infection.py
+++ infection.py
@@ -1,5 +1,28 @@
 import random
 import matplotlib.pyplot as plt
+'''
+This is a simulation project in which I wanted to simulate the following: 
+A large corporation has N employees each with a computer device (N is an even integer 2 ≤ N ≤ 1,000,000).
+
+Every day all employees exchange 3 messages with three other employees (assume
+that the targeted employees are selected at random). One of the employees was not cautious and started spreading an acquired computer virus through their email messages.
+
+Every person who receives the message follows these rules:
+1. The likelihood of acquiring and spreading the computer virus to another employee is 40%.
+2. After a person receives the virus 2 times, they are alerted and will take action to no longer spread it further.
+
+•	For different values of N = {100, 1000, 10000}, run your simulation several times to
+determine:
+o	On average, what % of the attendees will have heard the rumor after 3 days?
+o	On average, what % of the attendees will have heard the rumor after 20 days?
+o	On average, what % of the attendees will have heard the rumor after 40 days?
+•	 For N= 10,000
+o	After how many days will 10% of the employee devices be affected by the virus?
+o	After how many days will 50% of the employee devices be affected by the virus?
+
+
+
+'''
 
 def infection(days, N, rng):
     heard = [False] * N
